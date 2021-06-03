7b2760c6959289ef56c7d2787cebc97eb1d284e6
Andrey Lyubchenko <andrey.lyubchenko26@gmail.com>
2021-06-03 17:59:50 +0300

    secondReview

--- src/main/java/com/GoIt/TelegramBot/TextMessageBot.java
+++ src/main/java/com/GoIt/TelegramBot/TextMessageBot.java
@@ -15,114 +15,139 @@ import java.io.IOException;
 import java.util.*;
 
 public class TextMessageBot extends TelegramLongPollingBot {
-    Map<Long, TelegramUser> telegramUserMap = new HashMap<>();
+    Map<Long, TelegramUser> userMap = new HashMap<>();
+
     @Override
     public String getBotUsername() {
-        return TelegramConstants.BOT_NAME;
+        return TelegramCondition.BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return TelegramConstants.BOT_TOKEN;
+        return TelegramCondition.BOT_TOKEN;
     }
 
-    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            Long chatId = update.getMessage().getChatId();
-            String message = update.getMessage().getText();
-            if ("/start".equals(message)) {
-                if (!telegramUserMap.containsKey(chatId)) {
-                    getStartMessage(update);
-                } else {
-                    sendMessage(chatId, "Вы уже зарегестрированы " + telegramUserMap.get(chatId).getEmail());
-                }
-                return;
-            }
-            if (getValidEmail(update)) {
-                getTelegramUser(update);
-                return;
-            }
-            getInformation(update);
-        }
+        userRegistration(update);
 
     }
+
+    private void sendMessage(long chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(Long.toString(chatId));
+        sendMessage.setText(text);
+        firstKeyboardsMarkup(sendMessage);
+        try {
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void message(long chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(Long.toString(chatId));
+        sendMessage.setText(text);
+        try {
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    private void keyboardMessage(long chatId, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(Long.toString(chatId));
+        sendMessage.setText(text);
+        keyboardsMarkup(sendMessage);
+        try {
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startMessage(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        sendMessage(chatId, "Привет!\n" +
+                "\n" +
+                "Я Telegram бот, и я помогу подготовиться к собеседованию по JavaScrip");
+        sendMessage(chatId, "Вижу, ты новенький. Для начала нажми на кнопку Зарегистрироваться, чтобы я мог сохранять твой прогресс.\n" +
+                "\n" +
+                "Если не видишь кнопки - нажми на (::), чтобы открыть клавиатуру");
+    }
+
+    private void userRegistration(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String returnMessage = update.getMessage().getText();
+        switch (returnMessage) {
+            case "/start":
+                if (!userMap.containsKey(chatId)) {
+                    startMessage(update);
+                } else {
+                    message(chatId, "Вы уже зарегистрированы, ваш логин " + userMap.get(chatId).getEmail());
+                }
+                break;
+            case "Зарегистрироваться":
+                message(chatId, "Пожалуйста, введите ваш email.");
+                break;
+        }
+        if (getValidEmail(update)) {
+            getTelegramUser(update);
+            message(chatId, "Благодарим за регистрацию.");
+            keyboardMessage(chatId,"Выберите, что вас интересует.");
+
+        }
+    }
+
+    private void firstKeyboardsMarkup(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setSelective(true);
+
+        KeyboardRow keyboardButtons = new KeyboardRow();
+        keyboardButtons.add("Зарегистрироваться");
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        keyboardRows.add(keyboardButtons);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+
     private void getTelegramUser(Update update) {
         String message = update.getMessage().getText();
         Long chatId = update.getMessage().getChatId();
         TelegramUser telegramUser = new TelegramUser();
         telegramUser.setEmail(message);
-        telegramUserMap.put(chatId, telegramUser);
-        getEmail(update);
+        userMap.put(chatId, telegramUser);
     }
 
     private boolean getValidEmail(Update update) {
-        String message = update.getMessage().getText();
-        return message.matches("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}$");
-    }
-    private void getQuestionsAndAnswerFile(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        sendMessage(chatId, "https://docs.google.com/spreadsheets/d/1JAV0NxS2ZNH6IzrFLJumwTdU6uA_Fs37cX6Nu31W9fY/edit#gid=0");
-    }
-
-    private void getStartMessage(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        sendMessage(chatId, "Тебя приветствует JavaScriptBot.\nЧтобы пользоваться полноценно этим суперПупер ботом, тебе нужна регистрация, иначе иди и смотри мультики.");
-    }
-
-    private void getEmail(Update update) {
-        Long chatId = update.getMessage().getChatId();
         String text = update.getMessage().getText();
-        sendMsgAfterRegLogin(chatId, "Ваша электронная почта: " + text);
+        return text.matches("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}$");
+
+
     }
+    private void keyboardsMarkup(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
-    private synchronized void sendMsgAfterRegLogin(long chatId, String text) {
-        SendMessage sendMessageRequest = new SendMessage();
-        sendMessageRequest.setChatId(Long.toString(chatId));
-        sendMessageRequest.setText(text);
-        getReplayKeyBoardMarkup(sendMessageRequest);
-        try {
-            sendApiMethod(sendMessageRequest);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private synchronized void sendMessage(long chatId, String text) {
-        SendMessage sendMessageRequest = new SendMessage();
-        sendMessageRequest.setChatId(Long.toString(chatId));
-        sendMessageRequest.setText(text);
-        try {
-            sendApiMethod(sendMessageRequest);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void getReplayKeyBoardMarkup(SendMessage sendMessage) {
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-
-        sendMessage.setReplyMarkup(keyboardMarkup);
-        keyboardMarkup.setSelective(true);
-        keyboardMarkup.setResizeKeyboard(true);
-        keyboardMarkup.setOneTimeKeyboard(false);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        replyKeyboardMarkup.setResizeKeyboard(false);
+        replyKeyboardMarkup.setSelective(true);
 
         KeyboardRow keyboardButtons = new KeyboardRow();
-        keyboardButtons.add("/Вопросы и ответы по JavaScript");
-
+        keyboardButtons.add("Вопросы/Ответы\n-текстовый вариант");
+        keyboardButtons.add("Вопросы/Ответы\n-видео вариант");
+        keyboardButtons.add("Настроить напоминание");
+        keyboardButtons.add("Просмотреть погоду,\nвсегда есть о чем поговорить:)");
 
         List<KeyboardRow> keyboardRows = new ArrayList<>();
         keyboardRows.add(keyboardButtons);
-        keyboardMarkup.setKeyboard(keyboardRows);
-    }
 
-    private void getInformation(Update update) throws IOException {
-        switch (update.getMessage().getText()) {
-            case "/Вопросы и ответы по JavaScript":
-                getQuestionsAndAnswerFile(update);
-                break;
-
-        }
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
     }
 }
