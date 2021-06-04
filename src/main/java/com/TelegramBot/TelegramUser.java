c73e4028d9405e8208a4647c418cd90b77109397
Maksim <36765932+Skymori@users.noreply.github.com>
2021-06-04 15:25:49 +0300

    create User

--- src/main/java/com/TelegramBot/TelegramUser.java
+++ src/main/java/com/TelegramBot/TelegramUser.java
@@ -1,2 +1,13 @@
-package com.TelegramBot;public class TelegramUser {
+package com.TelegramBot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TelegramUser {
+    private String chatId;
+    private String name;
+    private String lastname;
+    private String email;
 }
