065b397708cfe856eb845d700eba4d599c21ee0f
Maksim <36765932+Skymori@users.noreply.github.com>
2021-06-04 21:23:00 +0300

    create test

--- src/main/java/com/TelegramBot/TelegramUser.java
+++ src/main/java/com/TelegramBot/TelegramUser.java
@@ -1,2 +1,54 @@
-package com.TelegramBot;public class TelegramUser {
+package com.TelegramBot;
+import lombok.Data;
+
+
+@Data
+
+public class TelegramUser {
+    private String name;
+    private String surName;
+    private int age;
+    private int id;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+    
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
