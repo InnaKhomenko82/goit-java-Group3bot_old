c94986d27f3dd70e4085b00ecaf0a20eba9d9baa
zeka <allrent3@gmail.com>
2021-06-06 17:47:47 +0300

    refresh17:47

--- src/main/java/com/TelegramBot/GoogleApi/GoogleApiToJson.java
+++ src/main/java/com/TelegramBot/GoogleApi/GoogleApiToJson.java
@@ -19,26 +19,23 @@ import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.*;
 
-// https://docs.google.com/spreadsheets/d/1JAV0NxS2ZNH6IzrFLJumwTdU6uA_Fs37cX6Nu31W9fY/edit#gid=0
-//   1JAV0NxS2ZNH6IzrFLJumwTdU6uA_Fs37cX6Nu31W9fY
-
-//  https://docs.google.com/spreadsheets/d/1z19e59hOvB1UoycPYnAzNEPSIUkSEswdOTY3jF_x5JM/edit#gid=1067596464
-//   1z19e59hOvB1UoycPYnAzNEPSIUkSEswdOTY3jF_x5JM
 public class GoogleApiToJson {
     private static final String CREDENTIALS_FILE = "/credential.json";
     private static final String PROPERTIES_FILE = "/application.properties";
     private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
     private static Properties appProp;
+    private static Sheets service;
+    private static ValueRange response5;
+
     static List<Object> values2 = new ArrayList<>();
     static List<Object> rows = new ArrayList<>();
-    static List<Object> response51Values = new ArrayList<>();
+    static List<Object> responseValues = new ArrayList<>();
     static List<Object> row = new ArrayList<>();
-    static Sheets service;
+
+
     static String blockName;
     static String blockName1;
 
-    static ValueRange response5;
 
     static List<String> blocksNames = new ArrayList<>();
     static List<Object> question = new ArrayList<>();
@@ -58,17 +55,17 @@ public class GoogleApiToJson {
 
         List<Sheet> sheets = spreadsheetMetadata.getSheets();
 
-       // sheets.forEach(sheet -> ((SheetProperties) (sheet.get("properties"))).get("title"));
-getBlock(sheets, spreadSheetId);
-getLessons(sheets,spreadSheetId);
+        // sheets.forEach(sheet -> ((SheetProperties) (sheet.get("properties"))).get("title"));
+
+        getBlock(sheets, spreadSheetId);
+        getLessons();
     }
-    private static void getLessons(List<Sheet> sheets,String spreadSheetId) throws IOException {
+
+    private static void getLessons() throws IOException {
         List<List<Object>> response5Values = response5.getValues();
-
         int count = 1;
-
         for (int i = 3; i < response5Values.size(); i = i + 2) {
-            response51Values.add(response5Values.get(i));
+            responseValues.add(response5Values.get(i));
             blockName1 = (String) response5Values.get(0).get(0);
             String a = (String) response5Values.get(i).get(0);
             String b = (String) response5Values.get(i).get(1);
@@ -76,44 +73,36 @@ getLessons(sheets,spreadSheetId);
             question.add("\nВопрос №" + a);
             ask.add("\nОтвет №" + count + ". " + b);
             videoURL.add("\nСсылка на видео к уроку №" + count + ". " + c);
-
-            // allBloc.add(" \n Урок №"+count+" \nВопрос№"+a+" \nОтвет: "+b+" \nСсылка на видео: "+c);
-            lessons.add(" \n Урок №"+count+"\nВопрос №" + a + "\nОтвет №" + count + ". " + b + "\nСсылка на видео к уроку №" + count + ". " + c);
+            lessons.add(" \n Урок №" + count + "\nВопрос №" + a + "\nОтвет №" + count + ". " + b + "\nСсылка на видео к уроку №" + count + ". " + c);
             count++;
-
-            row.add("\n"+a+"\n"+b+"\n"+c);
+            row.add("\n" + a + "\n" + b + "\n" + c);
             rows.add(row);
+            // allBloc.add(" \n Урок №"+count+" \nВопрос№"+a+" \nОтвет: "+b+" \nСсылка на видео: "+c);
             // String allR = "\nВопрос: " + r + " \nОтвет: " + r1 + " \nВидео: " + r2 + "\n";
             //String all2 = " Вопрос: " + r + " Ответ: " + r1 + " Видео: " + r2;
-          //   System.out.println(lessons);
-
-
         }
         System.out.println(row);
-        WriteToJson.main(ask);
-        WriteToJson.main(values2);
-        WriteToJson.main(rows);
+
+        WriteToJson.Writer2ToString(row);
+        WriteToJson.Writer1ToArray(responseValues);
+        WriteToJson.Writer3ToObject(values2);
     }
- private static void getBlock(List<Sheet> sheets,String spreadSheetId) throws IOException {
-     for (int i = 0; i < sheets.size(); i++) {
-         if (sheets != null) {
 
-             blockName = sheets.get(i)
-                     .getProperties()
-                     .getTitle();
-             blocksNames.add(blockName);
-             // System.out.println(title); // nameDocList - JavaScrip назание Гугл-Листа слева в углу
-             response5 = service
-                     .spreadsheets()
-                     .values()
-                     .get(spreadSheetId, blocksNames.get(0))////!A1:A-название блока обучения
-                     .execute();
-             values2.add(response5);
+    private static void getBlock(List<Sheet> sheets, String spreadSheetId) throws IOException {
+        for (Sheet sheet : sheets) {
+            blockName = sheet
+                    .getProperties()
+                    .getTitle();
+            blocksNames.add(blockName);// blockName - назание Гугл-Листа
+            response5 = service
+                    .spreadsheets()
+                    .values()
+                    .get(spreadSheetId, blocksNames.get(0)) //blocksNames.get(0)-номер лисат//!A1:A-название блока обучения
+                    .execute();
+            values2.add(response5);
+        }
+    }
 
-         }
-     }
-//     System.out.println(blocksNames.get(0));
- }
     private static HttpRequestInitializer getCredentials() throws IOException {
         InputStream in = GoogleApiToJson.class.getResourceAsStream(CREDENTIALS_FILE);
         if (in == null) {
--- src/main/java/com/TelegramBot/GoogleApi/WriteToJson.java
+++ src/main/java/com/TelegramBot/GoogleApi/WriteToJson.java
@@ -6,71 +6,34 @@ import com.google.gson.GsonBuilder;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class WriteToJson extends GoogleApiToJson {
-    public static void main(List<Object> args) throws IOException {
-//      Writer1ToArray(rows);
-//      Writer2ToString(values2);
-//      Writer3ToObject(response5Values);
+
+    public static void Writer1ToArray(Object responseValues) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(ask);  //  done
+        String json = gson.toJson(responseValues);
         File file = new File("src/main/resources/GoogleToJsonArray.json");
         FileWriter writer = new FileWriter(file);
         writer.write(String.valueOf(json));
-        String json2 = gson.toJson(ask);
-        File file2 = new File("src/main/resources/GoogleToJsonToString1.json");
+        writer.flush();
+        writer.close();
+    }
+    public static void Writer2ToString(Object row) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json2 = gson.toJson(row);
+        File file2 = new File("src/main/resources/GoogleToJsonToString.json");
         FileWriter writer2 = new FileWriter(file2);
         writer2.write(String.valueOf(json2));
-        String json3 = gson.toJson(ask);
+        writer2.flush();
+        writer2.close();
+    }
+    public static void Writer3ToObject(Object values2) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json3 = gson.toJson(values2);
         File file3 = new File("src/main/resources/GoogleToJsonToObject.json");
         FileWriter writer3 = new FileWriter(file3);
         writer3.write(String.valueOf(json3));
-        //System.out.println(json);
-
-        writer.flush();
-        writer.close();
-
+        writer3.flush();
+        writer3.close();
     }
-//    public static void Writer1ToArray(Object rows) throws IOException {
-//        // System.out.println(rows);
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        String json = gson.toJson(rows);  //rows1
-//        String json2 = gson.toJson(values2);
-//        String json3 = gson.toJson(response5Values);
-//        //System.out.println(json);
-//        File file = new File("src/main/resources/GoogleToJsonArray.json");
-//        File file2 = new File("src/main/resources/GoogleToJsonToString.json");
-//        File file3 = new File("src/main/resources/GoogleToJsonToObject.json");
-//        FileWriter writer = new FileWriter(file);
-//        FileWriter writer2 = new FileWriter(file2);
-//        FileWriter writer3 = new FileWriter(file3);
-//        writer.write(String.valueOf(json));
-//        writer2.write(String.valueOf(json2));
-//        writer3.write(String.valueOf(json3));
-//        writer.flush();
-//        writer.close();
-//    }
-//    public static void Writer2ToString(Object values2) throws IOException {
-//        // System.out.println(rows);
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        String json2 = gson.toJson(values2);
-//        //System.out.println(json);
-//        File file2 = new File("src/main/resources/GoogleToJsonToString.json");
-//        FileWriter writer2 = new FileWriter(file2);
-//        writer2.write(String.valueOf(json2));
-//        writer2.flush();
-//        writer2.close();
-//    }
-//    public static void Writer3ToObject(Object response5Values) throws IOException {
-//        // System.out.println(rows);
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        String json3 = gson.toJson(response5Values);
-//        //System.out.println(json);
-//        File file3 = new File("src/main/resources/GoogleToJsonToObject.json");
-//        FileWriter writer3 = new FileWriter(file3);
-//        writer3.write(String.valueOf(json3));
-//        writer3.flush();
-//        writer3.close();
-//    }
 }
