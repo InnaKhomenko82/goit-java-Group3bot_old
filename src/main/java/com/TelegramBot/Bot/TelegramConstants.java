563d11eee595068e1bc1ad1f5b73d882e34466cd
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-06-08 22:58:29 +0300

    Delete TelegramConstants.java

--- src/main/java/com/TelegramBot/Bot/TelegramConstants.java
+++
@@ -1,6 +0,0 @@
-package com.TelegramBot.Bot;
-
-public class TelegramConstants {
-    public static final String BOT_NAME = "Bot";
-    public static final String BOT_TOKEN = "1735152814:AAHl-HswHHWoMpcC4YTzqq-DaHOypTBGpjk";
-}
